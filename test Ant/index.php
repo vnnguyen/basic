ae429fcb6b2846b7b89521528f124ff5357e30c5
Nguyen <nguyennv@amica-travel.com>
2020-06-17 13:56:55 +0700

    update

--- test Ant/index.php
+++ test Ant/index.php
@@ -221,8 +221,8 @@ function p($start, $not_yet_visited, $t, $distanc){
     }
     return $p;
 }
-public function actionAnt(){
-        $soDinh = 7;//so dinh
+    public function actionAnt(){
+        $soDinh = 8;//so dinh
         $start = 1;// diem bat dau
         $distanc = [];// luu do dai cac canh
         $t = [];//luu nong do vet
@@ -238,22 +238,10 @@ public function actionAnt(){
         $stop_loop = 2000;
         $n_loop = 0;
         $traveler = 1 * 25;
-        $last = 6;
+        $last = 1;
         $commback = true;
 
 
-
-        function resetMark($soDinh){
-            for($i = 1; $i <= $soDinh; $i ++){
-                $mark[$i] = false;
-            }
-            return $mark;
-        }
-        function resetUv($soDinh){
-            for($i = 1; $i <= $soDinh; $i ++){
-                $uv[$i] = 0;
-            }
-        }
 
         for($i = 1; $i <= $soDinh; $i ++){
             for($j = 1; $j <= $soDinh; $j ++){
@@ -265,18 +253,40 @@ public function actionAnt(){
                 }
             }
         }
-        $distanc[1][2] = $distanc[2][1] = 5;
-        $distanc[1][4] = $distanc[4][1] = 9;
-        $distanc[1][5] = $distanc[5][1] = 1;
-        $distanc[2][3] = $distanc[3][2] = 2;
-        $distanc[3][4] = $distanc[4][3] = 20;
-        $distanc[4][5] = $distanc[5][4] = 2;
-        $distanc[2][6] = $distanc[6][2] = 4;
-        $distanc[2][7] = $distanc[7][2] = 2;
-        $distanc[3][7] = $distanc[7][3] = 6;
+        $distanc[1][2] = $distanc[2][1] = 2; // HN -> HP 
+        $distanc[1][3] = $distanc[3][1] = 1.5; // HN -> VietTri
+        $distanc[1][4] = $distanc[4][1] = 2.75;  // HN -> HA LONG
+        $distanc[1][5] = $distanc[5][1]= 2; // HN -> Hoa Binh
+        $distanc[1][6] = $distanc[6][1]= 2.75; // HN-> LangSon
+        $distanc[1][7] = $distanc[7][1]= 6.5; // HN-> SonLa
+        $distanc[1][8] = $distanc[8][1]= 3.5; // HN-> Bac Kan
 
-        $distanc[4][7] = $distanc[7][4] = 3;
-        $distanc[6][7] = $distanc[7][6] = 1;
+        $distanc[2][3] = $distanc[3][2]= 2.75; // HP -> VietTri
+        $distanc[2][4] = $distanc[4][2]= 1.25; // HP -> Halong
+        // $distanc[2][5] = $distanc[5][2]= 3.25; // HP -> Hoa binh
+        // $distanc[2][6] = $distanc[6][2]= 3.5; // HP -> LangSon
+        $distanc[2][7] = $distanc[7][2]= 7.75; // HP -> Sơn La
+        $distanc[2][8] = $distanc[8][2]= 3.75; // HP -> bac Kan
+
+        $distanc[3][4] = $distanc[4][3]= 3; // Viet Tri -> Ha Long
+        $distanc[3][5] = $distanc[5][3]= 1.75; // Viet Tri => Hoa Binh
+        // $distanc[3][6]= $distanc[6][3]= 3; // Viet Tri => Lang Son
+        // $distanc[3][7]= $distanc[7][3]= 5.5; // Viet Tri => Son LA
+        $distanc[3][8]= $distanc[8][3]= 3; // Viet Tri => Bac Kan
+
+        $distanc[4][5] = $distanc[5][4]= 3.2; // Ha Long => Hoa Binh
+        // $distanc[4][6] = $distanc[6][4]= 3.4; // Ha Long => Lang son
+        // $distanc[4][7]= $distanc[7][4]= 8.2; // Ha Long => son La
+        $distanc[4][8]= $distanc[8][4]= 4.2; // Ha Long => bac Kan 
+
+        $distanc[5][6] = $distanc[6][5]= 3.75; // Hoa Binh => Lang Son
+        // $distanc[5][7] = $distanc[7][5]= 5; // Hoa Binh => Son La
+        $distanc[5][8]= $distanc[8][5]= 4; //Hoa binh => bac kan
+
+        $distanc[6][7] = $distanc[7][6]= 8.5; // Lang Son => Son La
+        $distanc[6][8] = $distanc[8][6]= 4; // LangSon => Bac Kan
+
+        $distanc[7][8] = $distanc[8][7]= 8.5; // Sơn La => Bac Kan
 
         for($i = 1; $i <= $soDinh-1; $i ++){
             for($j = $i + 1; $j <= $soDinh; $j ++){
@@ -363,11 +373,11 @@ public function actionAnt(){
             $n_loop ++;
             // echo 'loop '. $n_loop .'<br>';
             for ($K = 1; $K <= $traveler ; $K++) { 
-                $start = 6;
+                $start = 1;
                 $w = [];
                 $w[] = $start;
                 $cost = 0;
-                $visities = [3, 1, 4];
+                $visities = [2,3,4,5,6,7,8];
                 $not_yet_visited = array_values(array_diff($visities, $w));
                 // var_dump($not_yet_visited);die;
                 
@@ -442,7 +452,7 @@ public function actionAnt(){
         var_dump($cost_best);
         var_dump($best_router);
         var_dump($full_router);
-        var_dump($t);die;
+        die;
 
         
         
